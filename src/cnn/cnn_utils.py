04d9a26973b8b0aa1208c65a355bcf67de7f7a16
Eliasz Korecki <eliaszkorecki@gmail.com>
2026-04-20 20:50:02 +0200

    Opis w cnn_utils

--- src/cnn/cnn_utils.py
+++ src/cnn/cnn_utils.py
@@ -3,9 +3,16 @@ import torch
 from src.cnn.DetectionLosses import DetectionLosses
 
 
-def generate_anchors(base_size: float = 1.0,
-                     scales: list[float] = [0.03, 0.06, 0.09],
-                     aspect_ratio: list[float] = [0.75, 1.0, 1.25]) -> torch.Tensor:
+def generate_anchors(base_size=1.0, scales=[0.03, 0.06, 0.09], aspect_ratio=[0.75, 1.0, 1.25]):
+    """
+    Generuje zestaw anchorów (ramki referencyjne) o różnych skalach i proporcjach boków.
+    Zwraca tensor PyTorch zawierający szerokość i wysokość każdego anchoru.
+
+    Parametry:
+    - base_size (float): bazowy rozmiar, względem którego skalowane są anchory.
+    - scales (list[float]): lista współczynników skalowania określających wielkość anchorów.
+    - aspect_ratio (list[float]): lista proporcji szerokości do wysokości anchorów.
+    """
     anchors = []
     for scale in scales:
         for ratio in aspect_ratio:
@@ -16,9 +23,16 @@ def generate_anchors(base_size: float = 1.0,
 
 
 
-def non_maximum_suppression(boxes: torch.Tensor,
-                            scores: torch.Tensor,
-                            iou_threshold: float = 0.5) -> torch.Tensor:
+def non_maximum_suppression(boxes, scores, iou_threshold=0.5):
+    """
+    Wykonuje Non-Maximum Suppression (NMS) w celu usunięcia nakładających się ramek o niższych score’ach.
+    Zwraca indeksy wybranych ramek, które najlepiej reprezentują wykryte obiekty.
+
+    Parametry:
+    - boxes (Tensor): tensor ramek w formacie [N, 4], gdzie każda ramka to (x1, y1, x2, y2).
+    - scores (Tensor): tensor score’ów (pewności) dla każdej ramki o rozmiarze [N].
+    - iou_threshold (float): próg IoU, powyżej którego ramki są uznawane za nakładające się i odrzucane.
+    """
     indices = torch.argsort(scores, descending=True)
     keep = []
     while indices.numel() > 0:
@@ -27,10 +41,10 @@ def non_maximum_suppression(boxes: torch.Tensor,
 
         if indices.numel() == 1:
             break
-
+        
         remaining_boxes = boxes[indices[1:]]
         current_box = boxes[current].unsqueeze(0).repeat(len(remaining_boxes), 1)
         iou = DetectionLosses.compute_iou(current_box, remaining_boxes)
         indices = indices[1:][iou < iou_threshold]
-
+    
     return torch.tensor(keep, dtype=torch.long)
