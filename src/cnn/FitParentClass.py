cf73fae291b8a4c8f016af3ebb03c95b0d1493e9
Carlos11235813 <kubablu@op.pl>
2026-04-26 00:58:32 +0200

    Updated FitParentClass, now it make use of weights of losses + logs basic data about fit to wandb

--- src/cnn/FitParentClass.py
+++ src/cnn/FitParentClass.py
@@ -5,6 +5,7 @@ import wandb
 from src.cnn.BuildTargets import BuildTargets
 from src.cnn.DetectionLosses import DetectionLosses
 from src.callbacks.EarlyStoping import EarlyStopping
+from src.models.LossWeights import LossWeights
 
 class FitParentClass(nn.Module):
     def __init__(self):
@@ -13,13 +14,15 @@ class FitParentClass(nn.Module):
     def _train(self,
                 dataloader: torch.utils.data.DataLoader,
                 optimizer: torch.optim.Optimizer,
-                device: torch.device) -> float:
+                device: torch.device,
+                loss_weights: LossWeights) -> float:
         """
         Iterate once trough dataloader, and updates the weights in the model.
         It logs data to wandb.
         :param dataloader: DataLoader to iterate on
         :param optimizer: Optimizer used in training
         :param device: Specifies the device to run on
+        :param loss_weights: Loss weights used in training.
         :return: It returns the total loss on Dataloader.
         total loss == localization_loss + classify_loss + objectness_loss
         """
@@ -27,6 +30,9 @@ class FitParentClass(nn.Module):
         total_objectness = 0
         total_localization = 0
         total_classification = 0
+
+        user_defined = True if loss_weights is not None else False
+
         for images, yolo in dataloader:
             images = images.to(device)
             optimizer.zero_grad()
@@ -48,6 +54,16 @@ class FitParentClass(nn.Module):
             total_localization += loss_localization.item()
             total_classification += loss_classification.item()
 
+            if not user_defined:
+                loss_weights.auto_weights(loss_objectness,
+                                          loss_localization,
+                                          loss_classification)
+
+            loss_objectness = loss_weights.objectness * loss_objectness
+            loss_localization = loss_weights.localization * loss_localization
+            loss_classification = loss_weights.classification * loss_classification
+
+
             loss = loss_objectness + loss_localization + loss_classification
 
             loss.backward()
@@ -65,7 +81,8 @@ class FitParentClass(nn.Module):
 
     def _validate(self,
                   validation_dataloader: torch.utils.data.DataLoader,
-                  device: torch.device) -> float:
+                  device: torch.device,
+                  loss_weights: LossWeights = None) -> float:
         """
         Iterate once trough validation_dataloader, does not update the weights in the model.
         It mesures models performance on validation_dataloader.
@@ -80,6 +97,9 @@ class FitParentClass(nn.Module):
         total_objectness = 0
         total_localization = 0
         total_classification = 0
+
+        user_defined = True if loss_weights is not None else False
+
         with torch.no_grad():
             for images, yolo in validation_dataloader:
                 images = images.to(device)
@@ -94,6 +114,15 @@ class FitParentClass(nn.Module):
                 loss_objectness, loss_localization, loss_classification = DetectionLosses.compute_basic_loss(
                                                                                             outputs=outputs,
                                                                                             targets=targets)
+                if not user_defined:
+                    loss_weights.auto_weights(loss_objectness,
+                                              loss_localization,
+                                              loss_classification)
+
+                loss_objectness = loss_weights.objectness * loss_objectness
+                loss_localization = loss_weights.localization * loss_localization
+                loss_classification = loss_weights.classification * loss_classification
+
                 total_objectness += loss_objectness.item()
                 total_localization += loss_localization.item()
                 total_classification += loss_classification.item()
@@ -112,19 +141,36 @@ class FitParentClass(nn.Module):
             optimizer: torch.optim.Optimizer,
             train_loader: torch.utils.data.DataLoader,
             wandb_config: dict = None,
-            val_loader: torch.utils.data.DataLoader = None) -> None:
+            val_loader: torch.utils.data.DataLoader = None,
+            loss_weights: LossWeights = None) -> None:
         """
         Training loop that works for specified number of epochs.
         It validates model on validation dataloader, if it is specified.
 
 
-        :param epochs: Specifies the number of epochs to run
-        :param optimizer: Specifies the optimizer to use
-        :param train_loader: Specifies the dataloader to iterate on in training mode
-        :param wandb_config: Specifies the wandb configuration
-        :param val_loader: Specifies the dataloader to iterate on in validation mode
-        :return: None
+        :param epochs: Specifies the number of epochs to run.
+        :param optimizer: Specifies the optimizer to use.
+        :param train_loader: Specifies the dataloader to iterate on in training mode.
+        :param wandb_config: Specifies the wandb configuration.
+        :param val_loader: Specifies the dataloader to iterate on in validation mode.
+        :param loss_weights: Specifies the loss weights to use.
+        :return: None.
         """
+        wandb_config = wandb_config or {}
+
+        if loss_weights is None:
+            for k in LossWeights().__dict__.keys():
+                wandb_config["initial " + k + "weight"] = "auto"
+
+        else:
+            for k in loss_weights.__dict__.keys():
+                wandb_config["initial " + k + "weight"] = loss_weights.__dict__[k]
+
+        wandb_config["max epochs"] = epochs
+        wandb_config["optimizer"] = optimizer
+        wandb_config["train_loader length"] = len(train_loader)
+        wandb_config["val_loader length"] = len(val_loader)
+
         wandb.init(
             project="SCNN",
             config=wandb_config
@@ -137,7 +183,8 @@ class FitParentClass(nn.Module):
             self.train()
             loss = self._train(dataloader=train_loader,
                                optimizer=optimizer,
-                               device=device)
+                               device=device,
+                               loss_weights=loss_weights)
 
             print(f"Epoch {epoch + 1}/{epochs}, Train Total Loss: {loss}")
             if val_loader is not None:
