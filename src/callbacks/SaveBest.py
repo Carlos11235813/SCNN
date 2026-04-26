9b6e7c5b77d41c510b73ff6004c04bd63385ee8d
Carlos11235813 <kubablu@op.pl>
2026-04-26 02:43:46 +0200

    Added file extension for SaveBest

--- src/callbacks/SaveBest.py
+++ src/callbacks/SaveBest.py
@@ -5,8 +5,8 @@ import torch
 class SaveBest:
 
     def __init__(self, path: str = None):
-        self.path = path or os.path.join(os.getcwd(), "SaveBest_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
-        self.best_loss = float('inf')
+        self.path = path or os.path.join(os.getcwd(), "SaveBest_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + ".pth")
+        self.best_loss = float("inf")
 
     def __call__(self, model: torch.nn.Module, epoch: int, loss: float):
         if loss < self.best_loss:
