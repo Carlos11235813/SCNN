5ce67485b4535d185714f17f8c0098351c8da675
Carlos11235813 <kubablu@op.pl>
2026-05-05 18:23:16 +0200

    Added extension

--- src/cnn/VisDroneMobileNet.py
+++ src/cnn/VisDroneMobileNet.py
@@ -3,19 +3,19 @@ import torch.nn as nn
 from torchvision import models
 from src.cnn.FitParentClass import FitParentClass
 
-class VisDroneMobileNet(nn.Module):
+class VisDroneMobileNet(FitParentClass):
     def __init__(self, B_boxes: int = 1, C: int = 10):
 
         super().__init__()
         self.C = C
         self.B = B_boxes
-        
+
         out_channels = (self.B * 5) + self.C
 
         base_model = models.mobilenet_v3_small(weights=None)
-        
+
         self.backbone = base_model.features
-        
+
         self.detection_head = nn.Sequential(
             nn.Conv2d(576, 256, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(256),
