714ff29234ff07d55f38f2fd0ca1a7179add84e8
Carlos11235813 <kubablu@op.pl>
2026-05-06 19:05:22 +0200

    Updated docs in `log_latency`

--- src/wandb_logging/WandbLogger.py
+++ src/wandb_logging/WandbLogger.py
@@ -109,7 +109,15 @@ class WandbLogger:
         })
 
     @staticmethod
-    def log_latency(process: str, latencies: list):
+    def log_latency(process: str, latencies: list) -> None:
+        """
+        Computes and logs average latency and std of latencies.
+
+        :param process: Indicates type of losses, ex: process == \"Training Weighted\" or process == \"Validation Original\".
+        :param latencies:  list of latencies measured.
+        :return: None
+        """
+
         wandb.log({
             f"{process} Latency Mean [ms]": sum(latencies) / len(latencies),
             f"{process} Latency Std [ms]": torch.tensor(latencies).std().item(),
